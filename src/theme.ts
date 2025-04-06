570be01721500bed0ddb241e3a42edb10a804861
Zaki Amin <zakiamin0402@gmail.com>
2025-04-06 20:39:19 +0100

    style: add colour palette

--- src/theme.ts
+++ src/theme.ts
@@ -3,6 +3,14 @@
 import { createTheme } from '@mui/material/styles'
 
 const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3f6147',
+    },
+    secondary: {
+      main: '#7ce6dd',
+    },
+  },
   typography: {
     fontFamily: 'var(--font-roboto)',
   },
