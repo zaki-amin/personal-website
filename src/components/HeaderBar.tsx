551587f36646442f05ab7e9697f76b2f68528f94
Zaki Amin <zakiamin0402@gmail.com>
2025-04-06 21:31:48 +0100

    refactor: improve use of buttons and add icons

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,17 +1,37 @@
 import { FC } from 'react'
-import { AppBar, Button, Toolbar, Typography } from '@mui/material'
+import { AppBar, Button, ButtonGroup, Toolbar, Typography } from '@mui/material'
+import MenuBookIcon from '@mui/icons-material/MenuBook'
+import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople'
+import NewspaperIcon from '@mui/icons-material/Newspaper'
+import Stack from '@mui/material/Stack'
 
 const HeaderBar: FC = () => {
   return (
     <AppBar position='static'>
       <Toolbar>
-        <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+        <Typography variant='h5' component='div' sx={{ flexGrow: 1 }}>
           Zaki Amin
         </Typography>
-        <Button color='inherit'>Home</Button>
-        <Button color='inherit'>About</Button>
-        <Button color='inherit'>Projects</Button>
-        <Button color='inherit'>Contact</Button>
+        <ButtonGroup variant='contained' color='secondary'>
+          <Button color='secondary'>
+            <Stack direction='row' spacing={1}>
+              <span>about</span>
+              <NewspaperIcon />
+            </Stack>{' '}
+          </Button>
+          <Button color='secondary'>
+            <Stack direction='row' spacing={1}>
+              <span>portfolio</span>
+              <MenuBookIcon />
+            </Stack>
+          </Button>
+          <Button color='secondary'>
+            <Stack direction='row' spacing={1}>
+              <span>contact</span>
+              <EmojiPeopleIcon />
+            </Stack>
+          </Button>
+        </ButtonGroup>
       </Toolbar>
     </AppBar>
   )
