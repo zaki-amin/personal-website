aae084f3de185d6ad99ee15b273edc3cfb029392
Zaki Amin <zakiamin0402@gmail.com>
2025-04-15 21:45:22 +0100

    feat: add image to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import Image from 'next/image'
 import {
   NavigationMenu,
   NavigationMenuList,
@@ -9,15 +10,32 @@ import { icons } from 'lucide-react'
 
 export default function Home() {
   return (
-    <NavigationMenu className='p-4'>
-      <NavigationMenuList className={navigationMenuTriggerStyle()}>
-        <NavigationItem name='About' href='/about' icon={<icons.BadgeHelp />} />
-        <NavigationItem
-          name='Projects'
-          href='/project'
-          icon={<icons.WalletCards />}
-        />
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div className='flex flex-col items-center justify-center gap-4'>
+      <NavigationMenu className='p-4 mt-8'>
+        <NavigationMenuList className={navigationMenuTriggerStyle()}>
+          <NavigationItem
+            name='About'
+            href='/about'
+            icon={<icons.BadgeHelp />}
+          />
+          <NavigationItem
+            name='Projects'
+            href='/project'
+            icon={<icons.WalletCards />}
+          />
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <h1 className='text-4xl font-bold text-green-300'>Zaki Amin</h1>
+      <h3 className='text-2xl'>(zah - key ah - mean)</h3>
+
+      <Image
+        src='/zaki.jpg'
+        alt='Zaki Amin'
+        width={300}
+        height={300}
+        className='rounded-full'
+      />
+    </div>
   )
 }
