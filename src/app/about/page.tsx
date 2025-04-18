431b83904576af74865ab8a92c71ef2b248a604d
Zaki Amin <zakiamin0402@gmail.com>
2025-04-18 22:30:43 +0100

    feat: add simple hobbies section with music to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,6 +9,8 @@ import {
 } from '@/components/ui/breadcrumb'
 import { AboutIcon, HomeIcon } from '@/myComponents/IconWithText'
 import Education from '@/myComponents/about/Education'
+import { icons } from 'lucide-react'
+import Link from 'next/link'
 
 export const metadata = {
   title: 'Zaki Amin - About',
@@ -17,7 +19,7 @@ export const metadata = {
 
 export default function About() {
   return (
-    <div className='h-screen flex flex-col py-16 gap-12'>
+    <div className='h-screen flex flex-col py-16 gap-8'>
       <Breadcrumb className='px-16'>
         <BreadcrumbList>
           <BreadcrumbItem>
@@ -40,6 +42,31 @@ export default function About() {
 
       <Subsection title='Education'>
         <Education />
+      </Subsection>
+
+      <Subsection title='Hobbies'>
+        <ul className='list-disc pl-4'>
+          <li>
+            <span className='flex gap-2'>
+              Music <icons.Music />
+            </span>
+            <ul className='list-disc pl-4'>
+              <li>I play the flute in a concert band</li>
+              <li>
+                I sing, write and produce pop/R&B music which you can check out
+                on my{' '}
+                <Link
+                  href='https://open.spotify.com/artist/0RxI4VysFDQKWlBRzxbv5x?si=mIadSc6jRmq1cpeFqxd96A'
+                  target='_blank'
+                  className='text-green-500 hover:text-blue-400'
+                >
+                  Spotify
+                </Link>
+              </li>
+              <li>I am a bass in a classical choir</li>
+            </ul>
+          </li>
+        </ul>
       </Subsection>
     </div>
   )
