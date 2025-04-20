9503127ab5e0e2f4737ad6891d80bb7ee8417545
Zaki Amin <zakiamin0402@gmail.com>
2025-04-20 21:53:40 +0100

    feat: add simple links to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,8 @@
 import { ContactIcon } from '@/myComponents/IconWithText'
 import BreadcrumbWithIcon from '@/myComponents/navigation/BreadcrumbWithIcon'
 import PageHeader from '@/myComponents/layout/PageHeader'
+import Subsection from '@/myComponents/Subsection'
+import HoverLink from '@/myComponents/navigation/HoverLink'
 
 export const metadata = {
   title: 'Zaki Amin - Contact',
@@ -12,6 +14,20 @@ export default function About() {
     <div className='h-screen flex flex-col py-16 gap-8'>
       <BreadcrumbWithIcon icon={<ContactIcon />} />
       <PageHeader title='Contact' />
+
+      <Subsection title='Links'>
+        <ul className='list-disc pl-8'>
+          <li>
+            <HoverLink
+              href='https://www.linkedin.com/in/zaki-amin/'
+              text='LinkedIn'
+            />
+          </li>
+          <li>
+            <HoverLink href='https://github.com/zaki-amin' text='GitHub' />
+          </li>
+        </ul>
+      </Subsection>
     </div>
   )
 }
