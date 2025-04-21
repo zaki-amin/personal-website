54463462cd0e70e3c2e7c7f85d47ec59aec77ee1
Zaki Amin <zakiamin0402@gmail.com>
2025-04-21 21:45:50 +0100

    feat: describe previous roles

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -6,6 +6,7 @@ import PageHeader from '@/myComponents/layout/PageHeader'
 import CurrentRoleSummary from '@/myComponents/experience/CurrentRoleSummary'
 import TeachingResponsibilities from '@/myComponents/experience/TeachingResponsibilities'
 import PageFrame from '@/myComponents/layout/PageFrame'
+import PreviousRole from '@/myComponents/experience/PreviousRole'
 
 export const metadata = {
   title: 'Zaki Amin - Experience',
@@ -28,6 +29,21 @@ export default function Experience() {
 
         <Subsection title='Skills'>
           <SkillsBadges />
+        </Subsection>
+
+        <Subsection title='Previous roles'>
+          <div className='space-y-4 px-2'>
+            <PreviousRole
+              company='BidFX (now SGX FX)'
+              timespan='Apr - Aug 2023'
+              description='Software engineer intern at a foreign exchange fintech, focusing on distributed systems and continuous integration (CI) pipelines using Java. I worked on architecting microservices, implemented a reporting tool to collect and analyse test results, and refactored regression test suites including UI automation tests.'
+            />
+            <PreviousRole
+              company='BP'
+              timespan='Jun - Sep 2022'
+              description='Software engineer intern at an energy company, working on autonomous robotics for electric vehicles. In a small team, I designed and simulated an autonomous robotic arm to charge electric vehicles using Python and NVIDIA&#39;s Isaac Sim.'
+            />
+          </div>
         </Subsection>
       </div>
     </PageFrame>
