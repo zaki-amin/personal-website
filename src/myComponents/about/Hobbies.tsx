ffba674e73a279f9e9be2dac4b56222a2f9ab3d9
Zaki Amin <zakiamin0402@gmail.com>
2025-08-03 21:09:02 +0100

    feat: embellish content on hobbies r.e. city exploration

--- src/myComponents/about/Hobbies.tsx
+++ src/myComponents/about/Hobbies.tsx
@@ -11,9 +11,8 @@ const Hobbies: FC = () => {
         </span>
         <ul className='list-disc pl-4'>
           <li>I play the flute in a concert band</li>
-
           <li>
-            I sing, write and produce pop/R&B music which you can check out on
+            I sing, write and produce pop + R&B music which you can check out on
             my{' '}
             <HoverLink
               href='https://open.spotify.com/artist/0RxI4VysFDQKWlBRzxbv5x?si=mIadSc6jRmq1cpeFqxd96A'
@@ -22,6 +21,9 @@ const Hobbies: FC = () => {
             />
           </li>
         </ul>
+      </li>
+      <li>
+        I enjoy city exploration: photography, long walks, travelling and coffee
       </li>
     </ul>
   )
